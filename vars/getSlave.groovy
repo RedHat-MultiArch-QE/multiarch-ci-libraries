36eb7768c00626a134600e9b7a75d90ad3f12c20
Jeremy Poulin <jpoulin@redhat.com>
2017-12-12 14:12:28 -0500

    Moved provision and teardown into scripts, removed calls to job

--- vars/getSlave.groovy
+++ vars/getSlave.groovy
@@ -5,36 +5,16 @@
  *
  * @param arch String specifying the architecture of the slave to provision.
  * @param runOnProvisionedHost Boolean determining whether cinch should be run post provisioning.
+ * @param installAnsible Boolean determining whether ansible should be installed on the provisioned slave.
  * @return LinkedHashMap contained the name and buildNumber of the provisioned slave.
  */
-def LinkedHashMap call(String arch, Boolean runOnProvisionedHost) {
+def LinkedHashMap call(String arch, Boolean runOnProvisionedHost, def Boolean installAnsible = true) {
   def slave = [ buildNumber: null, arch: null, name: null, error: null ]
 
   try {
     stage('Provision Slave') {
-      println("Provisioning ${arch}-slave with runOnProvisionedHost=${runOnProvisionedHost}")
-
-      def buildResult = build([
-          job: '/multiarch-qe/provision-multiarch-slave',
-          parameters: [
-            string(name: 'ARCH', value: arch),
-            booleanParam(name: 'CONNECT_AS_SLAVE', value: runOnProvisionedHost)
-          ],
-          propagate: true,
-          wait: true
-        ])
-
-      // If provision was successful, you will be able to grab the build number
-      slave.buildNumber = buildResult.getNumber().toString()
-
-      // Get results of provisioning job
-      step([$class: 'CopyArtifact', filter: '*-slave.properties',
-          projectName: '/multiarch-qe/provision-multiarch-slave',
-          selector: [
-            $class: 'SpecificBuildSelector',
-            buildNumber: slave.buildNumber
-          ]
-        ])
+      println("Provisioning ${arch}-slave with runOnProvisionedHost=${runOnProvisionedHost} and installAnsible=${installAnsible}")
+      provisioner(arch, runOnProvisionedHost, installAnsible)
 
       // Load slave properties (you may need to turn off sandbox or approve this in Jenkins)
       slave = readProperties(file: "${arch}-slave.properties", defaults: slave)
@@ -42,8 +22,7 @@ def LinkedHashMap call(String arch, Boolean runOnProvisionedHost) {
     }
   } catch (e) {
     // If provision fails, grab the build number from the error message and set build status to not built
-    slave.buildNumber = ((e =~ "(provision-multiarch-slave #)([0-9]*)")[0][2])
-    currentBuild.result = 'NOT_BUILT'
+    currentBuild.result = 'FAILURE'
     slave.error = e.toString()
   } finally {
     println "Provisioned: ${slave}"
