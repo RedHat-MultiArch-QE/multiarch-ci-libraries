c8890b91edac2266988f8375bf12e9575f1f888b
Jeremy Poulin <jpoulin@redhat.com>
2017-12-14 13:50:23 -0500

    Continued ci libs refactor

---
+++ vars/parallelMultiArchTest.groovy
@@ -0,0 +1,38 @@
+/**
+ * parallelMultiArchTest.groovy
+ *
+ * Runs closure body on a multi-arch slave for each arch in arches param.
+ *
+ * @param arches LinkedList<String> specifying the arches to run single host tests on.
+ * @param runOnSlave Boolean that specificies whether the
+ *        closure should be run on directly on the provisioned slave.
+ * @param installAnsible Boolean that specificies whether Ansible should
+ *        be installed on the provisioned slave.
+ * @param test Closure that takes a slave object.
+ * @param onTestFailure Closure that takes an Exception parameter and the String architecture of the slave.
+ */
+import com.redhat.multiarch.ci.Task
+
+def call(LinkedList<String> arches, Boolean runOnSlave, Boolean installAnsible, Closure test, Closure onTestFailure) {
+  // Create arch Tasks to parallelize test
+  def LinkedList<Task> parallelTasks = []
+  for (arch in arches) {
+    parallelTasks.push(new Task(arch, { arch: arch }))
+  }
+
+  // Run single host test in parallel on each arch
+  parallelizeTasks(
+    parallelTasks,
+    { a ->
+      def String arch = new String(a)
+      return {
+        runTest(arch, runOnSlave, installAnsible, test, 
+          { 
+            e -> 
+            onTestFailure(e, a) 
+          }
+        )
+      }
+    }
+  )
+}
