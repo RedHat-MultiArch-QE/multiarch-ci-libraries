0332add7796deb49a8c9122e00f5f90f356f9c6d
Scott Hebert <shebert@redhat.com>
2018-01-09 15:29:37 -0500

    add provisioning Config defaults, ssh copy and fix args for destroy

--- src/com/redhat/multiarch/ci/ProvisioningConfig.groovy
+++ src/com/redhat/multiarch/ci/ProvisioningConfig.groovy
@@ -2,13 +2,13 @@ package com.redhat.multiarch.ci
 
 class ProvisioningConfig {
     // Job group for Beaker provisioning.
-    String jobgroup = null
+    String jobgroup = 'multiarch-qe'
     // name of Openshift tenant to pull provisioning image from.
-    String tenant = null
+    String tenant = 'redhat-multiarch-qe'
     // docker url to pull provisioning image from.
-    String dockerUrl = null
+    String dockerUrl = '172.30.1.1:5000'
     // name of provisioning image. Can include tag name.
-    String provisioningImage = null
+    String provisioningImage = 'provisioner'
     // Provisioning repo url
     String provisioningRepoUrl = null
     // Provisioning repo ref
@@ -17,11 +17,19 @@ class ProvisioningConfig {
     // This can reference a relative path in the above repo
     // or it can reference a relative path that already exists
     // in the current directory
-    String provisioningWorkspaceDir = null
+    String provisioningWorkspaceDir = 'workspace'
     // Kerberos principal for Beaker authentication.
-    String krbPrincipal = null
+    String krbPrincipal = 'jenkins/multiarch-qe-jenkins.rhev-ci-vms.eng.rdu2.redhat.com'
     // ID of Jenkins credential for keytab needed for Beaker authentication.
-    String KEYTABCREDENTIALID = null
+    String KEYTABCREDENTIALID = params.KEYTABCREDENTIALID ?: 'KEYTAB'
+    // ID of Jenkins credential for SSH private key to will be
+    // copied to provisioned resource.
+    // *** This must be the same as what was added to Beaker ***
+    String SSHPRIVKEYCREDENTIALID = params.SSHPRIVKEYCREDENTIALID ?: 'SSHPRIVKEY'
+    // ID of Jenkins credential for SSH public key to will be
+    // copied to provisioned resource
+    // *** This must be the same as what was added to Beaker ***
+    String SSHPUBKEYCREDENTIALID = params.SSHPUBKEYCREDENTIALID ?: 'SSHPUBKEY'
     // Whether the closure should be run on directly on the provisioned slave.
     Boolean runOnSlave = true
     // Whether Ansible should be installed on the provisioned slave.
