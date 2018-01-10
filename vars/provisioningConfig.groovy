a19a04521eb42c7a596eab845d34233faa7c9a31
Scott Hebert <shebert@redhat.com>
2018-01-10 11:24:20 -0500

    re-add params

--- vars/provisioningConfig.groovy
+++ vars/provisioningConfig.groovy
@@ -2,11 +2,11 @@ import com.redhat.multiarch.ci.ProvisioningConfig
 
 class provisioningConfig {
 
-    def create() {
+    def create(params) {
         def config = new ProvisioningConfig()
-        //config.KEYTABCREDENTIALID = params.KEYTABCREDENTIALID ?: 'KEYTAB'
-        //config.SSHPRIVKEYCREDENTIALID = params.SSHPRIVKEYCREDENTIALID ?: 'SSHPRIVKEY'
-        //config.SSHPUBKEYCREDENTIALID = params.SSHPUBKEYCREDENTIALID ?: 'SSHPUBKEY'
+        config.KEYTABCREDENTIALID = params.KEYTABCREDENTIALID ?: 'KEYTAB'
+        config.SSHPRIVKEYCREDENTIALID = params.SSHPRIVKEYCREDENTIALID ?: 'SSHPRIVKEY'
+        config.SSHPUBKEYCREDENTIALID = params.SSHPUBKEYCREDENTIALID ?: 'SSHPUBKEY'
         return config
     }
 }
