a52ca561f6b62a24473ddc8fe68f1d49b357e22b
Jeremy Poulin <jpoulin@redhat.com>
2018-01-22 16:28:42 -0500

    Provisioning config with env params rework

---
+++ src/com/redhat/multiarch/ci/Utilities.groovy
@@ -0,0 +1,15 @@
+package com.redhat.multiarch.ci
+
+class Utilities {
+  static def createProvisioningConfig(params, env) {
+    def config = new ProvisioningConfig()
+    config.KEYTABCREDENTIALID = params.KEYTABCREDENTIALID ?: config.KEYTABCREDENTIALID
+    config.SSHPRIVKEYCREDENTIALID = params.SSHPRIVKEYCREDENTIALID ?: config.SSHPRIVKEYCREDENTIALID
+    config.SSHPUBKEYCREDENTIALID = params.SSHPUBKEYCREDENTIALID ?: config.SSHPUBKEYCREDENTIALID
+    config.JENKINSSLAVECREDENTIALID = params.JENKINSSLAVECREDENTIALID ?: config.JENKINSSLAVECREDENTIALID
+    config.JENKINS_MASTER_URL = ${env.JENKINS_MASTER_URL} ?: config.JENKINS_MASTER_URL
+    config.JSWARM_EXTRA_ARGS = ${env.JSWARM_EXTRA_ARGS} ?: config.JSWARM_EXTRA_ARGS
+    return config
+  }
+}
+
