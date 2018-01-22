0973e70f87000ec42361606a39b8bf2c40cf2cd0
Jeremy Poulin <jpoulin@redhat.com>
2018-01-22 15:37:41 -0500

    Update provisioningConfig.groovy

--- vars/provisioningConfig.groovy
+++ vars/provisioningConfig.groovy
@@ -2,14 +2,14 @@ import com.redhat.multiarch.ci.ProvisioningConfig
 
 class provisioningConfig {
 
-    def create(params, env) {
+    def create(params, e) {
         def config = new ProvisioningConfig()
         config.KEYTABCREDENTIALID = params.KEYTABCREDENTIALID ?: config.KEYTABCREDENTIALID
         config.SSHPRIVKEYCREDENTIALID = params.SSHPRIVKEYCREDENTIALID ?: config.SSHPRIVKEYCREDENTIALID
         config.SSHPUBKEYCREDENTIALID = params.SSHPUBKEYCREDENTIALID ?: config.SSHPUBKEYCREDENTIALID
         config.JENKINSSLAVECREDENTIALID = params.JENKINSSLAVECREDENTIALID ?: config.JENKINSSLAVECREDENTIALID
-        //config.JENKINS_MASTER_URL = env.JENKINS_MASTER_URL ?: config.JENKINS_MASTER_URL
-        //config.JSWARM_EXTRA_ARGS = env.JSWARM_EXTRA_ARGS ?: config.JSWARM_EXTRA_ARGS
+        config.JENKINS_MASTER_URL = e.JENKINS_MASTER_URL ?: config.JENKINS_MASTER_URL
+        config.JSWARM_EXTRA_ARGS = e.JSWARM_EXTRA_ARGS ?: config.JSWARM_EXTRA_ARGS
         return config
     }
 }
