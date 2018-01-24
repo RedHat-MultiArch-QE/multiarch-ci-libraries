8cf8abd9084d4ab6c7d82e5909a7cd70f79f97fd
Jeremy Poulin <jpoulin@redhat.com>
2018-01-24 14:38:55 -0500

    Update TestUtils.groovy

--- vars/TestUtils.groovy
+++ vars/TestUtils.groovy
@@ -4,39 +4,45 @@ import com.redhat.multiarch.ci.test.*
 class TestUtils {
   static def config
   
-  static ProvisioningConfig getProvisioningConfig() {
+  static ProvisioningConfig getProvisioningConfig(script) {
     if (provisioningConfig) return provisioningConfig
-    config = new ProvisioningConfig(params, env)
+    config = new ProvisioningConfig(script.params, script.env)
   }
 
   /**
    * Runs @test on the multi-arch capable provisioner container,
    * an runs @onTestFailure if it encounter an Exception.
    *
+   * @param script Context for calling pipeline steps.
    * @param arch String specifying the arch to run tests on.
    * @param config ProvisioningConfig Configuration for provisioning.
    * @param test Closure that takes the Slave used by the test.
    * @param onTestFailure Closure that take the Slave used by the test and the Exception that occured.
    */
-  static def runTest(String arch,
-       ProvisioningConfig config,
-       Closure test,
-       Closure onTestFailure) {
+  static def runTest(
+    def script,
+    String arch,
+    ProvisioningConfig config,
+    Closure test,
+    Closure onTestFailure) {
     (new Test(this, arch, config, test, onTestFailure)).run()
   }
 
   /**
    * Run closure body on a multi-arch provisioned host for each arch in arches param.
    *
+   * @param script Context for calling pipeline steps.
    * @param arches List<String> specifying the arches to run single host tests on.
    * @param config ProvisioningConfig Configuration for provisioning.
    * @param test Closure that takes the Slave used by the test.
    * @param onTestFailure Closure that take the Slave used by the test and the Exception that occured.
    */
-  static def runParallelMultiArchTest(List<String> arches,
-       ProvisioningConfig config,
-       Closure test,
-       Closure onTestFailure) {
-    (new ParallelMultiArchTest(this, arches, config, test, onTestFailure)).run()
+  static def runParallelMultiArchTest(
+    def script,
+    List<String> arches,
+    ProvisioningConfig config,
+    Closure test,
+    Closure onTestFailure) {
+      (new ParallelMultiArchTest(this, arches, config, test, onTestFailure)).run()
+    }
   }
-}
