bcf259bbe6dc759dace0bf17b6119d02b7fb78b2
Jeremy Poulin <jpoulin@redhat.com>
2018-01-24 15:07:41 -0500

    Update Task.groovy

--- src/com/redhat/multiarch/ci/task/Task.groovy
+++ src/com/redhat/multiarch/ci/task/Task.groovy
@@ -7,16 +7,15 @@ class Task {
   /**
    * Calls a @body for each task in @tasks.
    *
-   * @param script Context to call pipeline steps.
    * @param tasks List<Task> to be parallelized.
    * @param body Closure that takes a single parameter representing a Map of arguments.
    */
-  static def parallelize(def script, List<Task> tasks, Closure body) {
+  static def parallelizeTaskList(List<Task> tasks, Closure body) {
     def parallelTasks = [:]
     for (task in tasks) {
       parallelTasks[task.name] = body(task.params)
     }
 
-    script.parallel parallelTasks
+    parallelTasks
   }
 }
