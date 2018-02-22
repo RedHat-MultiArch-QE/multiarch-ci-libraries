dda127ee8f01f3915c3d9e4646eacaccb856434d
Juri Lelli <juri.lelli@redhat.com>
2018-02-22 09:20:26 +0100

    vars: add getTaskId function
    Retrive task_id from a CI_MESSAGE.

---
+++ vars/getTaskId.groovy
@@ -0,0 +1,12 @@
+import groovy.json.*
+
+def call(String message='') {
+    println "CI_MESSAGE=${message}"
+    writeFile file: "message.json", text: message
+
+    def json = readJSON file: 'message.json'
+    tid = json['build'].task_id
+
+    return tid.toString()
+}
+
