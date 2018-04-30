dc49e8f9807504869ae1e1a3e1bd5a88bbe3e53e
Jeremy Poulin <jpoulin@redhat.com>
2018-04-30 16:47:47 -0400

    Updating version for release

--- src/com/redhat/multiarch/ci/provisioner/ProvisioningConfig.groovy
+++ src/com/redhat/multiarch/ci/provisioner/ProvisioningConfig.groovy
@@ -2,7 +2,7 @@ package com.redhat.multiarch.ci.provisioner
 
 class ProvisioningConfig {
   // Provisioner version
-  String version = 'dev-v0.4'
+  String version = 'v1.0'
   // Jenkins kubernetes cloud name
   String cloudName = 'openshift'
   // Job group for Beaker provisioning.
