ba043eda5e8072de3b12afdf8435d41e8cf4d7c5
Jeremy Poulin <jpoulin@redhat.com>
2018-04-30 16:41:46 -0400

    Update Host.groovy

--- src/com/redhat/multiarch/ci/provisioner/Host.groovy
+++ src/com/redhat/multiarch/ci/provisioner/Host.groovy
@@ -5,20 +5,20 @@ class Host {
   String arch = null
   // Name of the host
   String hostName = null
-  // Any error that occured during provisioning
+  // Any error that occurred during provisioning
   String error = null
   // Name of the host as it will appear in Jenkins
   String name = null
-  // Target of the linchpin PinFile
+  // Target of the Linchpin PinFile
   String target = null
   // Full path to the inventory file
   String inventory = null
-  // Whether provisioning initializion was successful
+  // Whether provisioning initialization was successful
   Boolean initialized = false
   // Whether provisioning was successful
   Boolean provisioned = false
-  // Whether connecting the host to Jenkins via cinch was successful
+  // Whether connecting the host to Jenkins via Cinch was successful
   Boolean connectedToMaster = false
-  // Whether installing ansible on the host was successful
+  // Whether installing Ansible on the host was successful
   Boolean ansibleInstalled = false
 }
