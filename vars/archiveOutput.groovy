eb7b4fbe887d0f61c11ecba594b1c36f04330800
Jeremy Poulin <jpoulin@redhat.com>
2018-05-07 16:55:56 -0400

    Converted util calls to variable steps

---
+++ vars/archiveOutput.groovy
@@ -0,0 +1,16 @@
+def call() {
+  try {
+    archiveArtifacts allowEmptyArchive: true, artifacts: "${params.TEST_DIR}/ansible-playbooks/**/artifacts/*", fingerprint: true
+    junit "${params.TEST_DIR}/ansible-playbooks/**/reports/*.xml"
+  }
+  catch (e) {
+    // We don't care if this step fails
+  }
+  try {
+    archiveArtifacts allowEmptyArchive: true, artifacts: "${params.TEST_DIR}/scripts/**/artifacts/*", fingerprint: true
+    junit "${params.TEST_DIR}/scripts/**/reports/*.xml"
+  }
+  catch (e) {
+    // We don't care if this step fails
+  }
+}
---
+++ vars/runTests.groovy
@@ -0,0 +1,13 @@
+def call() {
+  // Cinch Mode
+  if (config.runOnSlave) {
+    sh "ansible-playbook -i 'localhost,' -c local ${params.TEST_DIR}/ansible-playbooks/*/playbook.yml"
+    sh "for i in ${params.TEST_DIR}/scripts/*/test.sh; do bash \$i; done"
+
+    return
+  }
+
+  // SSH Mode
+  sh ". /home/jenkins/envs/provisioner/bin/activate; ansible-playbook -i '${host.inventory}' ${params.TEST_DIR}/ansible-playbooks/*/playbook.yml"
+  sh "for i in ${params.TEST_DIR}/scripts/*/test.sh; do ssh root@${host.hostName} < \$i; done"
+}
