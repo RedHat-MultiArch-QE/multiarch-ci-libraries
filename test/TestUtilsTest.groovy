042e0d5cd3cf32dd4cee5e02e16c4993395eb5e3
Jeremy Poulin <jpoulin@redhat.com>
2018-07-31 16:39:02 -0400

    Removed all of the libraries for testing pipelines since they did not support testing 'vars' as part of the compile path

--- test/TestUtilsTest.groovy
+++ test/TestUtilsTest.groovy
@@ -1,47 +1,27 @@
-import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
-import static com.lesfurets.jenkins.unit.global.lib.LocalSource.localSource
 import org.junit.Test
 import org.junit.Before
-import com.lesfurets.jenkins.unit.BasePipelineTest
-import com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration
+import com.redhat.ci.provisioner.ProvisioningConfig
 
 /**
  * Tests TestUtils API and whether or not a library is importable.
  */
-class TestUtilsTest extends BasePipelineTest {
-
-    private LibraryConfiguration library = null
+class TestUtilsTest extends Script {
+    private final Binding binding = new Binding()
 
     @Before
     void setUp() {
-        library = library()
-            .name('multiarch-ci-libraries')
-            .retriever(localSource('build/libs'))
-            .targetPath('build/libs')
-            .defaultVersion('master')
-            .allowOverride(false)
-            .implicit(false)
-            .build()
-        helper.registerSharedLibrary(library)
-
-        helper.baseScriptRoot = 'test/jobs/'
-        helper.scriptRoots += 'vars'
-        helper.registerAllowedMethod('checkout', [LinkedHashMap]) { m -> }
-        super.setUp()
-        binding.setVariable('params', [:])
-        binding.setVariable('env', [:])
-        binding.setVariable('scm', [:])
+        setBinding(binding)
+        binding.setProperty('params', [:])
+        binding.setProperty('env', [:])
     }
 
     @Test
-    void shouldLoadLibrary() {
-        assert(library != null)
+    void shouldGetProvisioningConfig() {
+        ProvisioningConfig config = TestUtils.getProvisioningConfig(this)
+        assert(config != null)
     }
-
-    @Test
-    void canRunBasicPipeline() {
-        runScript('test/jobs/basicPipeline.jenkins')
-        printCallStack()
-        assertJobStatusSuccess()
+    @SuppressWarnings('JUnitPublicNonTestMethod')
+    Object run() {
+        null
     }
 }
