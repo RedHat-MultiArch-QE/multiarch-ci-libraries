445c6eba15877af76c69d690ca0f4870184f8843
Jeremy Poulin <jpoulin@redhat.com>
2018-08-10 15:31:54 -0400

    Attempted to add a default ssh user for all ssh connections run by the Jenkins user

--- src/com/redhat/multiarch/ci/provisioner/Provisioner.groovy
+++ src/com/redhat/multiarch/ci/provisioner/Provisioner.groovy
@@ -202,8 +202,11 @@ class Provisioner {
         mkdir -p ~/.ssh
         cp ${script.SSHPRIVKEY} ~/.ssh/id_rsa
         cp ${script.SSHPUBKEY} ~/.ssh/id_rsa.pub
+        echo "Host *" | tee -a ~/.ssh/config
+        echo "    User jenkins" | tee -a ~/.ssh/config
         chmod 600 ~/.ssh/id_rsa
         chmod 644 ~/.ssh/id_rsa.pub
+        chmod 600 ~/.ssh/config
         eval "\$(ssh-agent -s)"
         ssh-add ~/.ssh/id_rsa
       """
@@ -215,7 +218,7 @@ class Provisioner {
       echo "pkgs.devel.redhat.com,10.19.208.80 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAplqWKs26qsoaTxvWn3DFcdbiBxqRLhFngGiMYhbudnAj4li9/VwAJqLm1M6YfjOoJrj9dlmuXhNzkSzvyoQODaRgsjCG5FaRjuN8CSM/y+glgCYsWX1HFZSnAasLDuW0ifNLPR2RBkmWx61QKq+TxFDjASBbBywtupJcCsA5ktkjLILS+1eWndPJeSUJiOtzhoN8KIigkYveHSetnxauxv1abqwQTk5PmxRgRt20kZEFSRqZOJUlcl85sZYzNC/G7mneptJtHlcNrPgImuOdus5CW+7W49Z/1xqqWI/iRjwipgEMGusPMlSzdxDX4JzIx6R53pDpAwSAQVGDz4F9eQ==" | sudo tee -a /etc/ssh/ssh_known_hosts
 
       echo "Host pkgs.devel.redhat.com" | sudo tee -a /etc/ssh/ssh_config
-      echo "IdentityFile /home/jenkins/.ssh/id_rsa" | sudo tee -a /etc/ssh/ssh_config
+      echo "    IdentityFile /home/jenkins/.ssh/id_rsa" | sudo tee -a /etc/ssh/ssh_config
 
       sudo yum install -y yum-utils git
       curl -L -O http://download.devel.redhat.com/rel-eng/internal/rcm-tools-rhel-7-server.repo
