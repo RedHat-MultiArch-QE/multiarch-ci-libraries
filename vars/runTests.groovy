aa13e495abee40f662516c31799ea92a6090149d
Jeremy Poulin <jpoulin@redhat.com>
2018-09-20 14:20:55 -0400

    New error handling for body errors

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -3,23 +3,49 @@ import com.redhat.ci.hosts.ProvisionedHost
 import com.redhat.ci.provisioner.Mode
 
 void call(ProvisioningConfig config, ProvisionedHost host) {
+    List<Exception> exceptions = []
+
     // JNLP Mode
     if (config.mode == Mode.JNLP) {
-        sh "ansible-playbook -i 'localhost,' -c local ${params.TEST_DIR}/ansible-playbooks/*/playbook.yml"
-        sh "for i in ${params.TEST_DIR}/scripts/*/test.sh; do bash \$i; done"
-
-        return
+        try {
+            sh "ansible-playbook -i 'localhost,' -c local ${params.TEST_DIR}/ansible-playbooks/*/playbook.yml"
+        } catch (e) {
+            exceptions.add(e)
+        }
+        try {
+            sh "for i in ${params.TEST_DIR}/scripts/*/test.sh; do bash \$i; done"
+        } catch (e) {
+            exceptions.add(e)
+        }
     }
 
     // SSH Mode
-    sh("""
-        . /home/jenkins/envs/provisioner/bin/activate;
-        ansible-playbook -i '${host.inventoryPath}' ${params.TEST_DIR}/ansible-playbooks/*/playbook.yml
-    """)
-    sh("""
-        for i in ${params.TEST_DIR}/scripts/*/test.sh
-            do ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-                -i ~/.ssh/id_rsa root@${host.hostname} < \$i
-        done
-    """)
+    if (config.mode == Mode.SSH) {
+        try {
+            sh("""
+                ls -a workspace;
+                . /home/jenkins/envs/provisioner/bin/activate;
+                ansible-playbook --key-file '~/.ssh/id_rsa' -i '${host.inventoryPath}' \
+                    ${params.TEST_DIR}/ansible-playbooks/*/playbook.yml;
+                ls -a workspace;
+            """)
+        } catch (e) {
+            exceptions.add(e)
+        }
+        try {
+            sh("""
+                    for i in ${params.TEST_DIR}/scripts/*/test.sh
+                        do ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+                            -i ~/.ssh/id_rsa root@${host.hostname} < \$i
+                    done
+                """)
+        } catch (e) {
+            exceptions.add(e)
+        }
+    }
+
+    exceptions.each {
+        e ->
+        script.error(e.message)
+    }
 }
