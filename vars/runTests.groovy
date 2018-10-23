2548dea0755f70513684fd63c0d3111d5af431fa
Jeremy Poulin <jpoulin@redhat.com>
2018-10-23 18:19:46 -0400

    Started adding support for output synchronization in SSH mode

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -3,42 +3,55 @@ import com.redhat.ci.hosts.ProvisionedHost
 import com.redhat.ci.provisioner.Mode
 
 void call(ProvisioningConfig config, ProvisionedHost host) {
+
+    private final String ACTIVATE_PROVISIONER = '. /home/jenkins/envs/provisioner/bin/activate;'
     List<Exception> exceptions = []
 
     // JNLP Mode
     if (config.mode == Mode.JNLP) {
+        // Run Playbooks
         try {
             sh "ansible-playbook -i 'localhost,' -c local ${params.TEST_DIR}/ansible-playbooks/*/playbook.yml"
         } catch (e) {
             exceptions.add(e)
         }
+        // Run tests
         try {
-            sh "for i in ${params.TEST_DIR}/scripts/*/test.sh; do bash \$i; done"
+            sh "for i in ${params.TEST_DIR}/scripts/*/test.sh; do bash \$i ${host.scriptParams}; done"
         } catch (e) {
             exceptions.add(e)
         }
+        // Collect Artifacts
+        // Do nothing here since artifacts are already on the host where the archive step will run
     }
 
     // SSH Mode
     if (config.mode == Mode.SSH) {
+        // Run Playbooks
         try {
             sh("""
-                . /home/jenkins/envs/provisioner/bin/activate;
-                ansible-playbook --key-file '~/.ssh/id_rsa' -i '${host.inventoryPath}' \
-                    ${params.TEST_DIR}/ansible-playbooks/*/playbook.yml;
+                ${ACTIVATE_PROVISIONER}
+                ansible-playbook -i '${host.inventoryPath}' ${params.TEST_DIR}/ansible-playbooks/*/playbook.yml
             """)
         } catch (e) {
             exceptions.add(e)
         }
+        // Run Scripts
         try {
             sh("""
-                    for i in ${params.TEST_DIR}/scripts/*/test.sh
-                        do ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-                            -i ~/.ssh/id_rsa root@${host.hostname} < \$i
-                    done
-                """)
+                ${ACTIVATE_PROVISIONER}
+                ansible-playbook -i '${host.inventoryPath}' ${config.SCRIPT_RUNNER_PLAYBOOK} \
+                    --extra_vars '{script_params:"${host.scriptParams}"}'
+            """)
         } catch (e) {
             exceptions.add(e)
+        }
+        // Collect Artifacts
+        try {
+            sh("""
+                ${ACTIVATE_PROVISIONER}
+                ansible-playbook -i '${host.inventoryPath}' ${config.ARTIFACT_COLLECTOR_PLAYBOOK}
+            """)
         }
     }
 
