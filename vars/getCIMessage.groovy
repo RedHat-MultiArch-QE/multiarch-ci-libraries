a978e97fe75af4aea52711618bffcd7a05dd4cb1
Jeremy Poulin <jpoulin@redhat.com>
2019-02-07 15:49:49 -0500

    Updating CI Message to have better error handling and a map return

--- vars/getCIMessage.groovy
+++ vars/getCIMessage.groovy
@@ -1,6 +1,9 @@
-String call(String message='') {
+Map call(String message) {
     final String CI_MESSAGE_FILE = 'message.json'
+    if (!message) {
+        return [:]
+    }
+    
     writeFile(file:CI_MESSAGE_FILE, text:message)
-
     readJSON(file:CI_MESSAGE_FILE)
 }
