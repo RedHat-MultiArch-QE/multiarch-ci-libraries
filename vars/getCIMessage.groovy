df53ac201512cf9e4bad91b8dac6a5885bfd10f5
Jeremy Poulin <jpoulin@redhat.com>
2019-02-07 14:21:55 -0500

    Codenarc narcing all over the new function

--- vars/getCIMessage.groovy
+++ vars/getCIMessage.groovy
@@ -1,7 +1,6 @@
 String call(String message='') {
     final String CI_MESSAGE_FILE = 'message.json'
-    writeFile(file:CI_MESSAGE_FILE, text:params.CI_MESSAGE)
+    writeFile(file:CI_MESSAGE_FILE, text:message)
 
-    Map json = readJSON(file:CI_MESSAGE_FILE)
-    return json
+    readJSON(file:CI_MESSAGE_FILE)
 }
