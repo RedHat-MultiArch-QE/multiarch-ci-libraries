db6cb9ec5505cc36bb87134afcbc9f3c2a833e34
Jeremy Poulin <jpoulin@redhat.com>
2019-02-25 13:15:05 -0500

    SSH mode support and remote target host update (#36)
    * Added lines to cover installing cert for RCM tools
    
    * Installing certs on host so we can pull down dev imgs
    
    * Ensuring backup cert install is still available
    
    * Fixed curl requests so that they will install certs correctly
    
    * Credentials should now be installed correctly on SSH mode hosts, and guaranteed install when rhpkg is specified
    
    * Moved script object into the final closure, so objects can be passed to it in SSH mode.
    
    * Updated logic to allow for non-root ssh mode, secret file injection, and env exports
    
    * Inject secret locations failed, so attemting to remove layer of indirection
    
    * Updating utils to once against attempt variable injection
    
    * Codenarc is a narc
    
    * Attempting to escape $ so that file in inject
    
    * Updated local install to mkdirs before copying files there
    
    * Removed Jenkins krb principal for files list
    
    * We should now install the correct rhpkg tools per the rhel host spec
    
    * Added overridable map for inventory vars
    
    * Fixed syntax for NoOpProvisioner inventory vars override
    
    * Renamed inventoryVars, updated tests, and ensured proper functioning for NoOpProvisioner
    
    * Variable naming convention consistency in LinchPin workspace
    
    * Reading hostname and ensuring only the hostname is picked up
    
    * Package name fix for RHEL-8
    
    * Renamed resource playbooks to indicate what they were
    
    * Fixed distribution version
    
    * Fixed ternary grouping
    
    * Fixing python package install on RHEL 8
    
    * Fixed path issue for renamed playbooks
    
    * RHEL 7 targets should no longer attempt to install python 3
    
    * Renamed test output file to runlog
    
    * CI message should now be cleaned up

--- src/com/redhat/ci/Utils.groovy
+++ src/com/redhat/ci/Utils.groovy
@@ -12,23 +12,34 @@ class Utils {
     private static final String SUDO = 'sudo '
     private static final String NO_SUDO = ''
     private static final String INSTALL_FILE = 'install.sh'
+    private static final String SSH_ARGS = '-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+    private static final String SSH_IDENTITY = '-i ~/.ssh/id_rsa'
+    private static final String SSH = "ssh ${SSH_ARGS} ${SSH_IDENTITY}"
+    private static final String SCP = "scp ${SSH_ARGS} ${SSH_IDENTITY}"
+    private static final String HOME = '/home/jenkins'
+    private static final String KEYTAB = 'KEYTAB'
+    private static final String KRB_PRINCIPAL = 'KRB_PRINCIPAL'
+    private static final String SSHPRIVKEY = 'SSHPRIVKEY'
+    private static final String SSHPUBKEY = 'SSHPUBKEY'
+    private static final String KRBCONF = 'KRBCONF'
+    private static final String BKRCONF = 'BKRCONF'
 
     /**
      * Attemps to install Ansible.
      */
     @SuppressWarnings('GStringExpressionWithinString')
     static void installAnsible(Script script, ProvisioningConfig config, ProvisionedHost host = null) {
-        genericInstall(script, config, host) {
+        genericInstall(config, host) {
             privileged, sh ->
             String sudo = privileged ? SUDO : NO_SUDO
-            sh("""
+            sh(script, """
                 ${sudo}yum install python2-devel openssl-devel libffi-devel -y &&
                 ${sudo}mkdir -p /home/jenkins &&
                 ${sudo}chown --recursive \${USER}:\${USER} /home/jenkins &&
                 ${sudo}pip install --upgrade pip &&
                 ${sudo}pip install --upgrade setuptools &&
                 ${sudo}pip install --upgrade ansible
-            """)
+            """, null)
             if (host == null) {
                 return
             }
@@ -40,21 +51,27 @@ class Utils {
      * Attempts to install SSH and Beaker credentials.
      */
     static void installCredentials(Script script, ProvisioningConfig config, ProvisionedHost host = null) {
-        genericInstall(script, config, host) {
+        genericInstall(config, host) {
             privileged, sh ->
             String sudo = privileged ? SUDO : NO_SUDO
             script.withCredentials([
-                script.file(credentialsId:config.keytabCredentialId, variable:'KEYTAB'),
+                script.file(credentialsId:config.keytabCredentialId, variable:KEYTAB),
                 script.usernamePassword(credentialsId:config.krbPrincipalCredentialId,
-                                        usernameVariable:'KRB_PRINCIPAL',
+                                        usernameVariable:KRB_PRINCIPAL,
                                         passwordVariable:''),
-                script.file(credentialsId:config.sshPrivKeyCredentialId, variable:'SSHPRIVKEY'),
-                script.file(credentialsId:config.sshPubKeyCredentialId,  variable:'SSHPUBKEY'),
-                script.file(credentialsId:config.krbConfCredentialId,    variable:'KRBCONF'),
-                script.file(credentialsId:config.bkrConfCredentialId,    variable:'BKRCONF'),
+                script.file(credentialsId:config.sshPrivKeyCredentialId, variable:SSHPRIVKEY),
+                script.file(credentialsId:config.sshPubKeyCredentialId,  variable:SSHPUBKEY),
+                script.file(credentialsId:config.krbConfCredentialId,    variable:KRBCONF),
+                script.file(credentialsId:config.bkrConfCredentialId,    variable:BKRCONF),
             ]) {
-                script.env.HOME = '/home/jenkins'
-                sh("""
+                script.env.HOME = HOME
+                Map context = [
+                    env:[HOME:HOME],
+                    files:[script.KEYTAB, script.SSHPRIVKEY,
+                           script.SSHPUBKEY, script.KRBCONF, script.BKRCONF],
+                ]
+
+                sh(script, """
                     ${sudo}yum install -y krb5-workstation
                     ${sudo}cp ${script.KRBCONF} /etc/krb5.conf
                     ${sudo}mkdir -p /etc/beaker
@@ -69,7 +86,7 @@ class Utils {
                     chmod 644 ~/.ssh/id_rsa.pub
                     eval "\$(ssh-agent -s)"
                     ssh-add ~/.ssh/id_rsa
-                """)
+                """, context)
                 if (host != null) {
                     host.credentialsInstalled = true
                 }
@@ -82,21 +99,38 @@ class Utils {
      */
     @SuppressWarnings('LineLength')
     static void installRhpkg(Script script, ProvisioningConfig config, ProvisionedHost host = null) {
-        genericInstall(script, config, host) {
+        genericInstall(config, host) {
             privileged, sh ->
             String sudo = privileged ? SUDO : NO_SUDO
-            sh("""
+            String distro = host ? host.distro : 'RHEL-7'
+            String variant = host ? host.variant : 'Server'
+            if (!distro || !variant || !distro.startsWith('RHEL')) {
+                script.echo("Installing rhpkg tool is not supported for distro=[${distro}] and variant=[${variant}]")
+                return
+            }
+            String osMajorVersion = distro.find('[0-9]+')
+            if (!osMajorVersion || osMajorVersion.toInteger() < 5 || osMajorVersion.toInteger() > 8) {
+                script.echo("RCM rhpkg tool is not available for distro=[${distro}]. Invalid major version=[${osMajorVersion}]")
+                return
+            }
+            sh(script, """
                 echo "pkgs.devel.redhat.com,10.19.208.80 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAplqWKs26qsoaTxvWn3DFcdbiBxqRLhFngGiMYhbudnAj4li9/VwAJqLm1M6YfjOoJrj9dlmuXhNzkSzvyoQODaRgsjCG5FaRjuN8CSM/y+glgCYsWX1HFZSnAasLDuW0ifNLPR2RBkmWx61QKq+TxFDjASBbBywtupJcCsA5ktkjLILS+1eWndPJeSUJiOtzhoN8KIigkYveHSetnxauxv1abqwQTk5PmxRgRt20kZEFSRqZOJUlcl85sZYzNC/G7mneptJtHlcNrPgImuOdus5CW+7W49Z/1xqqWI/iRjwipgEMGusPMlSzdxDX4JzIx6R53pDpAwSAQVGDz4F9eQ==" | ${sudo}tee -a /etc/ssh/ssh_known_hosts
 
                 echo "Host pkgs.devel.redhat.com" | ${sudo}tee -a /etc/ssh/ssh_config
                 echo "IdentityFile /home/jenkins/.ssh/id_rsa" | ${sudo}tee -a /etc/ssh/ssh_config
 
+                ${sudo}curl -o /etc/pki/ca-trust/source/anchors/RedHat_CA.crt -k -L https://password.corp.redhat.com/cacert.crt
+                ${sudo}curl -o /etc/pki/ca-trust/source/anchors/PnTDevOps_CA.crt -k -L https://engineering.redhat.com/Eng-CA.crt
+                ${sudo}curl -o /etc/pki/ca-trust/source/anchors/RH-IT-Root-CA.crt -k -L https://password.corp.redhat.com/RH-IT-Root-CA.crt
+                ${sudo}update-ca-trust extract
+
                 ${sudo}yum install -y yum-utils git
-                curl -L -O http://download.devel.redhat.com/rel-eng/internal/rcm-tools-rhel-7-server.repo
-                ${sudo}yum-config-manager --add-repo rcm-tools-rhel-7-server.repo
+
+                curl -L -o rcm-tools.repo http://download.devel.redhat.com/rel-eng/internal/rcm-tools-rhel-${osMajorVersion}-${variant.toLowerCase()}.repo
+                ${sudo}yum-config-manager --add-repo rcm-tools.repo
                 ${sudo}yum install -y rhpkg
                 git config --global user.name "jenkins"
-            """)
+            """, null)
             if (host != null) {
                 host.rhpkgInstalled = true
             }
@@ -108,11 +142,11 @@ class Utils {
      * If a provisioned host with a non-null displayName is passed in, the install step will be
      * attempted on that host; otherwise, the install with target the current node.
      */
-    static void genericInstall(Script script, ProvisioningConfig config, ProvisionedHost host, Closure installWrapper) {
+    static void genericInstall(ProvisioningConfig config, ProvisionedHost host, Closure installWrapper) {
         // Installation should occur on current node
         if (host == null) {
             installWrapper(NO_SUDO) {
-                shCommand ->
+                script, shCommand, context=[:] ->
                 script.sh(shCommand)
             }
             return
@@ -124,9 +158,9 @@ class Utils {
                 throw new ProvisioningException('Installing in SSH mode but displayName is invalid.')
             }
 
-            script.node(host.displayName) {
-                installWrapper(SUDO) {
-                    shCommand ->
+            installWrapper(SUDO) {
+                script, shCommand, context=[:] ->
+                script.node(host.displayName) {
                     script.sh(shCommand)
                 }
             }
@@ -139,12 +173,31 @@ class Utils {
                 throw new ProvisioningException('Installing in SSH mode but hostname is invalid.')
             }
 
-            installWrapper(NO_SUDO) {
-                shCommand ->
-                script.writeFile(file:INSTALL_FILE, text:shCommand)
-                String runCommandOnHost = 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' +
-                    "-i ~/.ssh/id_rsa root@${host.hostname} < ${INSTALL_FILE}"
-                script.sh(runCommandOnHost)
+            installWrapper(host.remoteUser == 'root' ? NO_SUDO : SUDO) {
+                script, shCommand, context=[:] ->
+                // Copy files onto target host
+                String files = ''
+                if (context && context.files) {
+                    for (file in context.files) {
+                        files += """
+                            $SSH ${host.remoteUser}@${host.hostname} mkdir -p \$(dirname "$file") &&
+                            $SCP $file ${host.remoteUser}@${host.hostname}:$file;\n
+                        """
+                    }
+                }
+                script.sh(files)
+
+                // Export env vars
+                String exports = ''
+                if (context && context.env) {
+                    for (envVar in context.env) {
+                        exports += "export ${envVar.key}=${envVar.value};\n"
+                    }
+                }
+
+                // Run the actual script
+                script.writeFile(file:INSTALL_FILE, text:"$exports $shCommand")
+                script.sh("$SSH ${host.remoteUser}@${host.hostname} < $INSTALL_FILE")
             }
         }
     }
