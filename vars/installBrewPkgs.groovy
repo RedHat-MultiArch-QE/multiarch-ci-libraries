50823604a68ffbb96ed6e8a8315b3f1786e0dfad
Jeremy Poulin <jpoulin@redhat.com>
2019-05-31 13:06:23 -0400

    Using generic install to ensure ansible version is installed to target host in SSH mode

--- vars/installBrewPkgs.groovy
+++ vars/installBrewPkgs.groovy
@@ -1,5 +1,8 @@
-void call(Map params, Boolean privileged = false) {
-    String sudo = privileged ? 'sudo' : ''
+import com.redhat.ci.Utils
+import com.redhat.ci.hosts.ProvisionedHost
+import com.redhat.ci.provisioner.ProvisioningConfig
+
+void call(Map params, Boolean privileged = false, ProvisioningConfig config = null, ProvisionedHost host = null) {
     Boolean taskRepoCreated = false
     if (params.CI_MESSAGE != '') {
         tid = getTaskId(params.CI_MESSAGE)
@@ -11,14 +14,22 @@ void call(Map params, Boolean privileged = false) {
     }
 
     if (taskRepoCreated == true) {
-        sh """
-            ${sudo} yum install -y yum-utils
-            URL=\$(cat task-repo.properties | grep TASK_REPO_URLS= | sed 's/TASK_REPO_URLS=//' | sed 's/;/\\n/g')
-            ${sudo} yum-config-manager --add-repo \${URL}
-            ${sudo} cat /etc/yum.repos.d/*download.eng.bos.redhat.com*
-            ${sudo} sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/*download.eng.bos.redhat.com*
-            echo "gpgcheck=0" | ${sudo} tee -a /etc/yum.repos.d/*download.eng.bos.redhat.com*
-            ${sudo} yum clean all
-        """
+        Utils.genericInstall(this, config ?: new ProvisioningConfig(), host) {
+            privilegedOverride, sh ->
+            context = [
+                env:[HOME:env.HOME],
+                files:['task-repo.properties'],
+            ]
+            String sudo = (privileged || privilegedOverride) ? 'sudo' : ''
+            sh("""
+                ${sudo} yum install -y yum-utils
+                URL=\$(cat task-repo.properties | grep TASK_REPO_URLS= | sed 's/TASK_REPO_URLS=//' | sed 's/;/\\n/g')
+                ${sudo} yum-config-manager --add-repo \${URL}
+                ${sudo} cat /etc/yum.repos.d/*download.eng.bos.redhat.com*
+                ${sudo} sed -i 's/gpgcheck=1/gpgcheck=0/g' /etc/yum.repos.d/*download.eng.bos.redhat.com*
+                echo "gpgcheck=0" | ${sudo} tee -a /etc/yum.repos.d/*download.eng.bos.redhat.com*
+                ${sudo} yum clean all
+            """, context)
+        }
     }
 }
